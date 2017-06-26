df9a8ffb9bc441da3212b3d33dc16ef583ab53fc
Viachaslau Marozau <viachaslau.marozau@datalex.com>
2017-06-26 16:56:09 +0300

    JMP Task14 Backend Memory Management and Garbage Collection

--- Task_14_Backend_Memory_Management_and_Garbage_Collection/src/Runner.java
+++ Task_14_Backend_Memory_Management_and_Garbage_Collection/src/Runner.java
@@ -1,10 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner
 {
-    private static List<Car> cars = new ArrayList<Car>();
-
     public static void main(String... args)
     {
 
+        for (int i = 0; ; i++)
+        {
+            List<String> list = generateList(1000);
+            list = null;
+            System.out.println("Iteration " + (i + 1) + " complete");
+        }
+    }
+
+    private static List<String> generateList(int count)
+    {
+        List<String> result = new ArrayList<String>();
+        for (int i = 0; i > count; i++)
+        {
+            result.add("String_" + i);
+        }
+        return result;
     }
 
 }
